f9f16fada499639cf1427e5dfc58e28b3b4b9c3a
ashwani-dc <ashwaniarunjith@gmail.com>
2021-11-22 20:56:46 +0530

    heroku deploy

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.secret_key = "manbearpig_MUDMAN888"
+app.secret_key = "manbearpig_MUDMAN889"
 
-@app.route("/hello")
+river_data=[
+	{'River':"EXZ",
+	'Data':"dss fsfdfsf fffdf"},
+	{'River':"ABC",
+	'Data':"FSGGG yjhnn fffdf"},
+]
+
+aquifer_data=[{
+	'Stationarity':'Yes',
+	'Trend':'No',
+	'Cyclic':'Yes'
+}]
+
+@app.route("/")
 def index():
 	flash("what's your name?")
 	return render_template("index.html")
 
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+
+
+@app.route("/river", methods=['POST', 'GET'])
+def river():
+	return render_template("river.html",river_data=river_data)
+
+@app.route("/spring", methods=['POST', 'GET'])
+def spring():
+	return render_template("spring.html",river_data=river_data)
+
+@app.route("/lake", methods=['POST', 'GET'])
+def lake():
+	return render_template("lake.html",river_data=river_data)
+
+@app.route("/aquifer", methods=['POST', 'GET'])
+def aquifer():
+	return render_template("aquifer.html",aquifer_data=aquifer_data)
